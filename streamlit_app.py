e04eca404aff679e2dbef5a33171e21860bfdf5e
snowflake777777 <141265005+snowflake777777@users.noreply.github.com>
2023-08-04 12:42:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,10 +35,15 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
+import streamlit
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+try:
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_cur = my_cnx.cursor()
+    my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+    my_data_row = my_cur.fetchone()
+    streamlit.text("Hello from Snowflake:")
+    streamlit.text(my_data_row)
+except Exception as e:
+    streamlit.error(f"Error connecting to Snowflake: {e}")
+
